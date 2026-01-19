7a0a24ef4d8341cddefe896c6595b76a14e1f6ae
helloashishghimire <ashishghmr8848@gmail.com>
2026-01-19 16:09:16 -0600

    Initial Commit

---
+++ src/main/java/com/devops/health/HealthApiApplication.java
@@ -0,0 +1,12 @@
+package com.devops.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthApiApplication.class, args);
+    }
+}
